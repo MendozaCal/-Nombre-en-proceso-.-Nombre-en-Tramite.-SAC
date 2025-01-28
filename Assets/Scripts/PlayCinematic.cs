2fb7630c47ce6ef9f8f09e4a7ba3926942d31300
Francisco <francisco.anticona81@gmail.com>
2025-01-28 07:55:45 -0500

    Progress Tutorial and Cinematic

---
+++ Assets/Scripts/PlayCinematic.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PlayCinematic : MonoBehaviour
+{
+    [SerializeField] private Movement movementScript;
+    [SerializeField] private Grab grabScript;
+    [SerializeField] private PlayerLife PlayerLife;
+    [SerializeField] private Hang hangScript;
+    [SerializeField] private WallClimbing wallClimbingScrpt;
+
+    private PlayableDirector playableDirector;
+
+    private void Start()
+    {
+        playableDirector = GetComponent<PlayableDirector>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DeactivateSpecificScripts();
+            playableDirector.Play(); 
+        }
+    }
+
+    public void DeactivateSpecificScripts()
+    {
+        movementScript.enabled = false;
+        grabScript.enabled = false;
+        PlayerLife.enabled = false;
+        hangScript.enabled = false;
+        wallClimbingScrpt.enabled = false;
+    }
+
+    public void ActivateSpecificScripts()
+    {
+        movementScript.enabled = true;
+        grabScript.enabled = true;
+        PlayerLife.enabled = true;
+        hangScript.enabled = true;
+        wallClimbingScrpt.enabled = true;
+    }
+}
