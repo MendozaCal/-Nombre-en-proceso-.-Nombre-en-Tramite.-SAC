276464791901e5a37dfc32f29905001f23fb207d
Pablo Mendoza <147556543+MendozaCal@users.noreply.github.com>
2025-02-17 19:25:08 -0500

    Level4Actualization

--- Assets/Scripts/EnemiesScripts/TurtleScripts/AreaPersecution.cs
+++ Assets/Scripts/EnemiesScripts/TurtleScripts/AreaPersecution.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class AreaPersecution : MonoBehaviour
 {
-    [SerializeField]GameObject Turtle;
-    TortoiseMove TurtleMove;
-    [SerializeField] GameObject RutePoints;
+    [SerializeField] private GameObject Turtle;
+    private TortoiseMove TurtleMove;
+    [SerializeField] private Transform PointA;
+    [SerializeField] private Transform PointB;
+    private SphereCollider sphereCollider;
     public bool detectedPlayerArea;
+
     private void Start()
     {
         TurtleMove = Turtle.GetComponent<TortoiseMove>();
-        SphereCollider sphereCollider = GetComponent<SphereCollider>();
-        DetectorPatrollToAttack detectorPatrollToAttack = RutePoints.GetComponent<DetectorPatrollToAttack>();
-        sphereCollider.radius = detectorPatrollToAttack.distance;
+        sphereCollider = GetComponent<SphereCollider>();
+
+        if (PointA == null || PointB == null)
+        {
+            Debug.LogError("Faltan referencias a PointA o PointB en el Inspector.");
+            return;
+        }
+
+        // Calcular el centro entre PointA y PointB
+        Vector3 centerPosition = (PointA.position + PointB.position) / 2f;
+
+        // Centrar el collider en el punto medio (convertido a coordenadas locales)
+        sphereCollider.center = transform.InverseTransformPoint(centerPosition);
+
+        // Ajustar el radio del collider en base a la distancia entre los puntos
+        sphereCollider.radius = Vector3.Distance(PointA.position, PointB.position) / 2f;
     }
-    
+
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -23,6 +39,7 @@ public class AreaPersecution : MonoBehaviour
             detectedPlayerArea = true;
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
