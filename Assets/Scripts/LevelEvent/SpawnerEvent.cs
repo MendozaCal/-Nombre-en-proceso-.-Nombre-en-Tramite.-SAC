0ef0a39ba3a7392acb313e2090c3bca797954e84
Alonzzoh <edalopp@hotmail.com>
2025-02-19 00:27:26 -0500

    event lvl 4

--- Assets/Scripts/LevelEvent/SpawnerEvent.cs
+++ Assets/Scripts/LevelEvent/SpawnerEvent.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class SpawnerEvent : MonoBehaviour
 {
-    public GameObject cubePrefab; 
+    public List<GameObject> spawnPrefabs; 
     public float spawnInterval = 1f;
     public Vector3 spawnAreaSize = new Vector3(10f, 0f, 10f); 
-    
-
+    public float launchForce = 10f; 
     private float timeSinceLastSpawn;
 
     void Start()
@@ -22,13 +21,15 @@ public class SpawnerEvent : MonoBehaviour
 
         if (timeSinceLastSpawn >= spawnInterval)
         {
-            SpawnCube();
+            SpawnObject();
             timeSinceLastSpawn = 0f;
         }
     }
 
-    void SpawnCube()
+    void SpawnObject()
     {
+
+        GameObject prefabToSpawn = spawnPrefabs[Random.Range(0, spawnPrefabs.Count)];
 
         Vector3 randomPosition = new Vector3(
             Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2),
@@ -36,18 +37,24 @@ public class SpawnerEvent : MonoBehaviour
             Random.Range(-spawnAreaSize.z / 2, spawnAreaSize.z / 2)
         );
 
+        Vector3 spawnPosition = transform.position + transform.rotation * randomPosition;
 
-        Vector3 spawnPosition = transform.position + randomPosition;
-        Instantiate(cubePrefab, spawnPosition, Quaternion.identity);
-        
+        GameObject spawnedObject = Instantiate(prefabToSpawn, spawnPosition, transform.rotation);
+
+        Rigidbody rb = spawnedObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(transform.forward * launchForce, ForceMode.Impulse); 
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(0, 1, 0, 0.5f);
-        Gizmos.DrawWireCube(transform.position, spawnAreaSize);
+
+        Matrix4x4 oldMatrix = Gizmos.matrix;
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        Gizmos.DrawWireCube(Vector3.zero, spawnAreaSize);
+        Gizmos.matrix = oldMatrix;
     }
-
-
 }
-
