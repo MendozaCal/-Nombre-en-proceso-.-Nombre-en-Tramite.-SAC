8cb334cf63ebed301d3ad9a6c8c7a80a6a16c192
Francisco <francisco.anticona81@gmail.com>
2025-03-21 23:56:03 -0500

    Remove lines

--- Assets/Scripts/PlayerScripts/PlayerMecanics/Movement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMecanics/Movement.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
